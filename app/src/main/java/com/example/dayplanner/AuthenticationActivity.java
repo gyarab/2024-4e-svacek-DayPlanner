54147a7b16c06ef2cc250c324b4cbbda813d7e4a
Jan Sváček <jan@svacek.eu>
2025-01-23 12:25:09 +0100

    Login using email and password

--- app/src/main/java/com/example/dayplanner/AuthenticationActivity.java
+++ app/src/main/java/com/example/dayplanner/AuthenticationActivity.java
@@ -3,8 +3,10 @@ package com.example.dayplanner;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AuthenticationActivity extends AppCompatActivity {
 
     Button googleButton, facebookButton, emailButton;
+    TextView textViewLogin;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -52,5 +55,15 @@ public class AuthenticationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        textViewLogin = findViewById(R.id.textview_login);
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AuthenticationActivity.this, EmailLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/dayplanner/EmailLoginActivity.java
@@ -0,0 +1,81 @@
+package com.example.dayplanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class EmailLoginActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText;
+    Button loginButton, signInButton;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_email_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        emailEditText = findViewById(R.id.edit_email);
+        passwordEditText = findViewById(R.id.edit_password);
+        loginButton = findViewById(R.id.btn_login);
+        signInButton = findViewById(R.id.btn_signin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth = FirebaseAuth.getInstance();
+
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if((email.length() == 0) || (password.length() == 0)) {
+                    Toast.makeText(EmailLoginActivity.this, "Email or Password is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(EmailLoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(EmailLoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EmailLoginActivity.this, AuthenticationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
