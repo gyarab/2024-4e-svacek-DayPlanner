7560527dc5322bc5fb2f35da1f51e85f7633bde2
honzasvacek <jan.svacek@student.gyarab.cz>
2025-01-27 21:03:31 +0100

    facebook login almost done

---
+++ app/src/main/java/com/example/dayplanner/auth/facebook/FacebookAuthActivity.java
@@ -0,0 +1,26 @@
+package com.example.dayplanner.auth.facebook;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.dayplanner.R;
+
+public class FacebookAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_facebook_auth);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
